13ab9722c309ff6781e7a643231c99454963d45c
Rodrigo Ferreira <61603333+rodrigoferreirasj@users.noreply.github.com>
2025-12-15 10:50:26 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Isso é crucial para sua estrutura /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
